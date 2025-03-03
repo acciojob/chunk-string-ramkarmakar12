fad90e108f8a2c88fb502158cc29baa895fc911a
Ramkrishna Karmakar <53562477+ramkarmakar12@users.noreply.github.com>
2025-03-03 20:35:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 function stringChop(str, size) {
-  // your code here
+ 
+  if (str === null) {
+    return [];
+  }
+
+
+  const chunks = [];
+
+  
+  for (let i = 0; i < str.length; i += size) {
+    // Extract a chunk of the specified size
+    const chunk = str.slice(i, i + size);
+    // Add the chunk to the array
+    chunks.push(chunk);
+  }
+
+
+  return chunks;
 }
 
 // Do not change the code below
